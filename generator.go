3c20ab9cb72370142aa616d4e66069182c5a81e4
Fabrizio Lallai <fabrizio.lallai@boq.com.au>
2018-03-19 11:59:18 +1000

    Read doctype

--- generator.go
+++ generator.go
@@ -2,42 +2,85 @@ package doctype2go
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"os"
-	"github.com/jmoiron/jsonq"
+	"strings"
 )
+
+type docTypeField struct {
+	Type       string `json:"fieldtype"`
+	MappedType string
+	Required   bool   `json:"reqd"`
+	JSONName   string `json:"fieldname"`
+	Generate   bool
+}
 
 type template struct {
 	PkgName        string
 	WithLinkStruct bool
-	TypeName       string
-	TypeFields     []struct {
-		Name      string
-		Type      string
-		Decorator string
-	}
+	TypeName       string         `json:"name"`
+	TypeFields     []docTypeField `json:"fields"`
+}
+
+func (d *docTypeField) Name() (s string) {
+	s = strings.Replace(d.JSONName, "_", " ", -1)
+	s = strings.Title(s)
+	return strings.Replace(s, " ", "", -1)
 }
 
 // Generate generate the GO structure file from doctype
-func Generate(in *os.File, out *os.File, pkgName string) (err error) {
-	data := map[string]interface{}{}
-	dec := json.NewDecoder(in)
-	if err = dec.Decode(&data); err != nil {
+func Generate(in *os.File, out *os.File, pkgName string, withLinkStruct bool) (err error) {
+	t := getTemplate(in)
+
+	t.PkgName = pkgName
+	t.WithLinkStruct = withLinkStruct
+	hasLinks := false
+	typeMapping := map[string]string{
+		"Data":     "string",
+		"Select":   "string",
+		"Check":    "bool",
+		"Currency": "float32",
+		"Text":     "string",
+		"Float":    "float32",
+	}
+
+	for _, field := range t.TypeFields {
+		field.MappedType = typeMapping[field.Type]
+		if len(field.MappedType) > 0 {
+			field.Generate = true
+		}
+
+		if !hasLinks && field.Type == "Link" {
+			hasLinks = true
+		}
+	}
+
+	if hasLinks {
+		link := docTypeField{
+			JSONName:   "links",
+			MappedType: "[]Link",
+			Generate:   true,
+		}
+		t.TypeFields = append(t.TypeFields, link)
+	}
+
+	return
+}
+
+func getTemplate(in *os.File) (t* template) {
+	// data := map[string]interface{}{}
+	// dec := json.NewDecoder(in)
+	// if err = dec.Decode(&data); err != nil {
+	// 	return
+	// }
+	raw, err := ioutil.ReadAll(in)
+	if err != nil {
 		return
 	}
 
-	jq := jsonq.NewQuery(data)
-	fields, err := jq.ArrayOfObjects("fields")
-
-	fields["fieldname"]
-	fields["fieldtype"]
-	fields["reqd"]
-
-	var contact Contact
-	if err = json.Unmarshal([]byte(body), &contact); err != nil {
-		contacts = append(contacts, contact)
-	}
-
+	// jq := jsonq.NewQuery(data)
+	// f, err := jq.String("fields")
+	// var fields []docTypeField
+	err = json.Unmarshal([]byte(raw), &t)
 	return
 }
